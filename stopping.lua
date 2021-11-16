aea35567f3224ca717c55548572b21e3854fe805
fruiche <fruitcakesyndrome256@gmail.com>
2021-11-16 20:26:07 +0900

    Update stopping.lua
    一つのcontrolerにしか作用しなかったメソッドを修正

--- stopping.lua
+++ stopping.lua
@@ -15,18 +15,22 @@ function stopping(duration)
     while true do
         print("wait train...")
         event_name, net_address, augment_type, stock_uuid = event.pull("ir_train_overhead")
-            
+
         if augment_type == "LOCO_CONTROL" then
             print("come train")
             stoptime = computer.uptime()
-            component.proxy(net_address).setThrottle(0)
-            component.proxy(net_address).setBrake(1)
+            for net_address in component.list("ir_augment_control") do
+                component.proxy(net_address).setThrottle(0)
+                component.proxy(net_address).setBrake(1)
+            end
             while computer.uptime() - stoptime < duration do
                 os.sleep(1)
             end
             print("start train")
-            component.proxy(net_address).setThrottle(0.5)
-            component.proxy(net_address).setBrake(0)
+            for net_address in component.list("ir_augment_control") do
+                component.proxy(net_address).setThrottle(0.5)
+                component.proxy(net_address).setBrake(0)
+            end
             print("cool time")
             os.sleep(30) --30秒間クールタイム
         else
